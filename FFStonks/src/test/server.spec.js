f3a1b0aea6192236451cf044058caa8a22c7129a
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2023-04-19 22:05:25 -0600

    test cases and such (register still busted)

--- FFStonks/src/test/server.spec.js
+++ FFStonks/src/test/server.spec.js
@@ -28,15 +28,65 @@ describe('Server!', () => {
 
   //We are checking POST /add_user API by passing the user info in the correct order. This test case should pass and return a status 200 along with a "Success" message.
   //Positive cases
-  // it('positive : /add_user', done => {
-  //   chai
-  //     .request(server)
-  //     .post('/add_user')
-  //     .send({ id: 5, name: 'John Doe', dob: '2020-02-20' })
-  //     .end((err, res) => {
-  //       expect(res).to.have.status(200);
-  //       expect(res.body.message).to.equals('Success');
-  //       done();
-  //     });
-  // });
+  it('positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 'john27', password: 'yughjiuo7t6rydfgcvhjui'})
+      .end((err, res) => {
+        expect(res.body.status).to.equal('success')
+        done();
+      });
+  });
+
+  it('negative : /register (TO BE FIXED)', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 'john27', password: 'john'}) // will try to make account that already exists
+      .end((err, res) => {
+        expect(res.body.status).to.equals('failed');
+        done();
+      });
+  });
+
+  it('positive : /login', done => {
+    chai
+    .request(server)
+      .post('/login')
+      .send({ username: 'john27', password: 'yughjiuo7t6rydfgcvhjui' })
+      .end((err, res) => {
+        //expect(res).to.have.status(200); // 200 status code on success only
+        // .redirects() did not work, expect literal URL
+        expect(res.body.status).to.equal('success');
+        done();
+      });
+    });
+    
+  it('negative : /login : bad password', done => {
+    chai
+    .request(server)
+    .post('/login')
+    .send({ username: 'john27', password: 'bad_password' })
+    .end((err, res) => {
+      expect(res).to.have.status(404); // status sent when password wrong
+      done();
+    });
+  });
+
+  it('negative : /login : user does not exist', done => {
+    chai
+    .request(server)
+    .post('/login')
+    .send({ username: 'jeff', password: 'pw' })
+    .end((err, res) => {
+      expect(res).to.have.status(410); // status sent when password wrong
+      done();
+    });
+  });
+
+
+
+
+
 });
